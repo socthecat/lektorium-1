f3b501f33a62e7d8c085707a950d7b070658d335
sociophobia <socthecat@gmail.com>
2019-10-12 23:04:50 +0300

    made some structural changes to hw1.js

--- hw1.js
+++ hw1.js
@@ -1,46 +1,33 @@
-function max(arr) {
-    let max = 0;
+function arrmath (mode, arr) {
+    let result = null;
     for (i = 0; i < arr.length; i++) {
-        //checks if an element is a number and isn't nan
-        if(isNaN(arr[i]) && arr[i] != arr[i]) {
+        if (isNaN(arr[i])) {
             continue;
-        }
-        else if (max < arr[i]) {
-            max = arr[i]
+        } else {
+            switch(mode) {
+                case 'min':
+                    if (result < arr[i]) {
+                        result = arr[i];
+                    }
+                    break;
+                case 'max':
+                    if (result > arr[i]) {
+                        result = arr[i];
+                    }
+                    break;
+                case 'sum':
+                    result += arr[i];
+                    break;
+                default:
+                    break;
+            }
         }
     }
-    return max;
-}
-
-function min(arr) {
-    let min = 0;
-    for (i = 0; i < arr.length; i++) {
-        //checks if an element is a number and isn't nan
-        if(isNaN(arr[i]) && arr[i] != arr[i]) {
-            continue;
-        }
-        else if (min > arr[i]) {
-            min = arr[i]
-        }
-    }
-    return min;
-}
-
-function sum(arr){
-    let sum = 0;
-    for (i = 0; i < arr.length; i++) {
-        //checks if an element is a number and isn't nan
-        if(arr[i] != arr[i]) {
-            continue;
-        }
-        else if (typeof(arr[i]) === "number") {
-            sum += arr[i];
-        }
-    }
-    return sum;
+    return result;
 }
 
 console.log("[3,'a',0,-5,1,44,-12,3,NaN,0,0,1,2,-3,-3,undefined,2,1,4,-2-3-1]");
-console.log(max([3,'a',0,-5,1,44,-12,3,NaN,0,0,1,2,-3,-3,undefined,2,1,4,-2-3-1]));
-console.log(min([3,'a',0,-5,1,44,-12,3,NaN,0,0,1,2,-3,-3,undefined,2,1,4,-2-3-1]));
-console.log(sum([3,'a',0,-5,1,44,-12,3,NaN,0,0,1,2,-3,-3,undefined,2,1,4,-2-3-1]));
+console.log(arrmath('max', [3, 'a', 0, -5, 1, 44, -12, 3, NaN, 0, 0, 1, 2, -3, -3, undefined, 2, 1, 4, -2 - 3 - 1]));
+console.log(arrmath('min', [3, 'a', 0, -5, 1, 44, -12, 3, NaN, 0, 0, 1, 2, -3, -3, undefined, 2, 1, 4, -2 - 3 - 1]));
+console.log(arrmath('sum', [3, 'a', 0, -5, 1, 44, -12, 3, NaN, 0, 0, 1, 2, -3, -3, undefined, 2, 1, 4, -2 - 3 - 1]));
+console.log(arrmath('abrakadabra', [3, 'nanachi', 0, -5, 1, 44, -12, 3, NaN, 0, 0, 1, 2, -3, -3, undefined, 2, 1, 4, -2 - 3 - 1]));
